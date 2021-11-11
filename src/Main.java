8147fcac3a58aa159fa524a8d6f4d5d8298dce4d
Anna Sotnikova <ruceba77@gmail.com>
2021-11-11 20:32:46 +0600

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-      int ticketPrice = 3_500;
-      int millRubles = 20;
-      int numberMill = ticketPrice / millRubles;
+        int ticketPrice = 3_500;
+        int millRubles = 20;
+        int numberMill = ticketPrice / millRubles;
         System.out.println(numberMill);
 
     }
